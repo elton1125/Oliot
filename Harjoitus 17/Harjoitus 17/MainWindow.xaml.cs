bb6aed56353bbfde42db11438bb03e2703b0249c
elton1125 <159885101+elton1125@users.noreply.github.com>
2024-09-03 10:09:32 +0300

    Harjoitus

---
+++ Harjoitus 17/Harjoitus 17/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// ¨Käytetty chatgpt apuna
+    public partial class MainWindow : Window
+    {
+        float lämpötila = 20;
+        bool keittiönValot = false;
+        bool olohuoneenValot = false;
+        bool oviLukossa = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PäivitäTilat();
+        }
+
+        private void PäivitäTilat()
+        {
+            tb_Lämpötila.Text = "Talon sisälämpötila on " + lämpötila;
+            tb_KeittiöTila.Text = keittiönValot.ToString();
+            tb_OlohuoneTila.Text = olohuoneenValot.ToString();
+            btn_Ovi.Content = oviLukossa.ToString();
+        }
+
+        private void btn_Olohuone_Click(object sender, RoutedEventArgs e)
+        {
+            olohuoneenValot = !olohuoneenValot;
+            PäivitäTilat();
+        }
+
+        private void btn_Keittiö_Click(object sender, RoutedEventArgs e)
+        {
+            keittiönValot = !keittiönValot;
+            PäivitäTilat();
+        }
+
+        private void btn_Ovi_Click(object sender, RoutedEventArgs e)
+        {
+            oviLukossa = !oviLukossa;
+            PäivitäTilat();
+        }
+
+        private void txt_Termostaatti_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txt_Termostaatti.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Numeroita");
+                txt_Termostaatti.Text = "";
+            }
+            else
+            {
+                if (txt_Termostaatti.Text == "")
+                    return;
+
+                lämpötila = float.Parse(txt_Termostaatti.Text);
+                PäivitäTilat();
+            }
+        }
+    }
+}
---
+++ Harjoitus 18/Harjoitus 18/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_18
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// Chatgpt käytetty apuna
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Summa_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double numero1 = Convert.ToDouble(txtBox_Numero1.Text);
+            double numero2 = Convert.ToDouble(txtBox_Numero2.Text);
+
+            
+            double tulos = numero1 + numero2;
+
+            
+            txtBlock_Tulos.Text = tulos.ToString();
+        }
+
+        private void btn_Erotus_Click(object sender, RoutedEventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtBox_Numero1.Text);
+            double numero2 = Convert.ToDouble(txtBox_Numero2.Text);
+            double tulos = numero1 - numero2;
+            txtBlock_Tulos.Text = tulos.ToString();
+        }
+
+        private void btn_Kertolasku_Click(object sender, RoutedEventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtBox_Numero1.Text);
+            double numero2 = Convert.ToDouble(txtBox_Numero2.Text);
+            double tulos = numero1 * numero2;
+            txtBlock_Tulos.Text = tulos.ToString();
+        }
+
+        private void btn_Jakolasku_Click(object sender, RoutedEventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtBox_Numero1.Text);
+            double numero2 = Convert.ToDouble(txtBox_Numero2.Text);
+
+            
+            if (numero2 != 0)
+            {
+                double tulos = numero1 / numero2;
+                txtBlock_Tulos.Text = tulos.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Nollalla ei voi jakaa!");
+            }
+        }
+    }
+}
