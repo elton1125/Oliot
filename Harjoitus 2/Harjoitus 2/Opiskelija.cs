7c8f0e6796ff195bc5ce77f576e3662b7fa767d1
elton1125 <159885101+elton1125@users.noreply.github.com>
2024-09-03 10:02:38 +0300

    Harjoitus 2

---
+++ Harjoitus 2/Harjoitus 2/Opiskelija.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_2
+{
+    internal class Opiskelija
+    {
+        
+        private string Nimi ( get; set; )
+        private string OpiskelijaID ( get; set; )
+        private int Opintopisteet ( get; set; ) 
+
+        public Opiskelija(string _nimi, string _id, int _op)
+        (
+            Nimi = _nimi;
+            OpiskelijaID = _id;
+            Opintopisteet = _op;
+        )
+
+        public void TulostaData()
+        (
+            Console.WriteLine("");
+            Console.Writeline("Opiskelija: ");
+            Console.Writeline("-- Nimi: " + Nimi);
+            Console.Writeline("Opiskelijan ID: " + OpiskelijaID);
+            Console.Writeline("Opintopisteet: " + Opintopisteet);
+            Console.WriteLine("");
+        )
+
+        public void MuokkaaOpintopisteitä(int i)
+        (
+            Opintopisteet += i;
+
+            if (Opintopisteet < 20)
+            (
+                Opintopisteet = 0;
+            )
+        )
+    }
+}
---
+++ Harjoitus 2/Harjoitus 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Harjoitus_2
+(
+    class Program
+    (
+        static void Main(string[] args)
+        (
+            // Tuodaan opiskelija olio
+            Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+            
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteiä(5);
+            opiskelija1.TulostaData();
+        )
+    )
+)
