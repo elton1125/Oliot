d875e016e8932ee89feec1108865bddd4ebdcc21
elton1125 <159885101+elton1125@users.noreply.github.com>
2024-09-03 10:03:00 +0300

    Harjoitus 3

---
+++ Harjoitus 3/Harjoitus 3/Kiuas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_3
+{
+    class Kiuas
+    {
+        private int Päällä;
+        private int Lämpötila;
+        private int Kosteus;
+
+        public Kiuas()
+        {
+            Päällä = false;
+            Lämpötila = 10;
+            Kosteus = 30;
+        }
+
+        public void LaitaPäälle()
+        {
+            Päällä = true;
+            Console.WriteLine("Kiuas päällä");
+        }
+
+        public void LaitaPois()
+        {
+            Päällä = false;
+            Console.WriteLine("Kiuas on pois päältä");
+        }
+
+        public void Vaihdalämpötila(int lämp)
+        {
+            Lämpötila = lämp;
+            Console.WriteLine("Asetettu lämpötila: (Lämpötila) astetta");
+        }
+
+        public void MuutaKosteus(int kos)
+        {
+            Kosteus = kos;
+            Console.WriteLine("Asetettu kosteus: (Kosteus");
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Kiuas päällä: (Päällä)");
+            Console.WriteLine("Lämpötila: (Lämpötila) astetta");
+            Console.WriteLine("Kosteus: (Kosteus)");
+        }
+    }
+}
---
+++ Harjoitus 3/Harjoitus 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas sauna = new Kiuas();
+            sauna.DisplayStatus();
+
+            Console.WriteLine("Kiuas päälle.");
+            sauna.LaitaPäälle();
+            sauna.Vaihdalämpötila(50);
+            sauna.MuutaKosteus(30);
+            sauna.DisplayStatus();
+
+            Console.WriteLine("Kiuas on pois päältä");
+            sauna.LaitaPois();
+            sauna.DisplayStatus();
+        }
+    }
+}
