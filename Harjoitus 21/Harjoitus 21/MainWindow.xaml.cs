cc56f3e2d99e17d2861782b1c14816771a705d28
elton1125 <159885101+elton1125@users.noreply.github.com>
2024-09-03 10:11:06 +0300

    Harjoitus 21

---
+++ Harjoitus 21/Harjoitus 21/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                
+                LisaaKarryyn(checkBox.Content.ToString());
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                // Poista tuote kärrystä
+                PoistaKarrysta(checkBox.Content.ToString());
+            }
+        }
+
+        private void OstaButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            checkBoxOmena.IsChecked = false;
+            checkBoxBanaani.IsChecked = false;
+            checkBoxAppelsiini.IsChecked = false;
+            VarastoPanel.Children.Clear(); 
+        }
+
+        private void LisaaKarryyn(string tuoteNimi)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = tuoteNimi;
+            VarastoPanel.Children.Add(textBlock);
+        }
+
+        private void PoistaKarrysta(string tuoteNimi)
+        {
+            foreach (var item in VarastoPanel.Children)
+            {
+                if (item is TextBlock textBlock && textBlock.Text == tuoteNimi)
+                {
+                    VarastoPanel.Children.Remove(textBlock);
+                    break;
+                }
+            }
+        }
+    }
+}
